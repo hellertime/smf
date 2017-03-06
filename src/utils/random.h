53e0a3504f41ef8ee8269c63b29e077aae2cbb03
Alexander Gallego <gallego.alexx@gmail.com>
2017-03-05 23:58:13 -0500

    random: use baseic_string<t> in random
    * compatibility w/ seastar sstring.hh and the
    standard std::string

--- src/utils/random.h
+++ src/utils/random.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <random>
+#include <string>
 
 #include <core/sstring.hh>
 
@@ -12,26 +13,26 @@ class random {
   }
 
   uint64_t next() { return dist_(rand_); }
-  sstring next_sstring(uint32_t size) {
-    static const sstring dict = "abcdefghijklmnopqrstuvwxyz"
-                                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                "1234567890"
-                                "!@#$%^&*()"
-                                "`~-_=+[{]}\\|;:'\",<.>/? ";
-    const static std::uniform_int_distribution<> dict_dist(0, dict.size());
 
-    sstring retval;
+  template <typename char_type = char>
+  std::basic_string<char_type> next_str(uint32_t size) {
+    static const std::string kDict = "abcdefghijklmnopqrstuvwxyz"
+                                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                     "1234567890"
+                                     "!@#$%^&*()"
+                                     "`~-_=+[{]}\\|;:'\",<.>/? ";
+    static const std::uniform_int_distribution<> kDictDist(0, dict.size());
+
+    std::basic_string<char_type> retval;
     retval.resize(size);
     // this could use a simd
     for (uint32_t i = 0; i < size; ++i) {
-      retval[i] = dict[dict_dist(rand_)];
+      retval[i] = kDict[kDictDist(rand_)];
     }
     return retval;
   }
 
  private:
-  // mersenne twister
-  // TODO(agallego) - replace w/ lemire's stuff
   std::mt19937 rand_;
   // by default range [0, MAX]
   std::uniform_int_distribution<uint64_t> dist_;
