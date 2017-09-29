52c0deceea9455e16cc6bd3b42873f4ecedc8701
Chris Heller <hellertime@gmail.com>
2017-09-29 09:47:07 -0400

    vagrant:build on ubuntu box
    Select Vagrant box with SMF_SYSTEM env var.
    Choices are 'ubuntu' or 'fedora' (default)
    Skips box update check (use `vagrant box update`)
    Drop boost python build for speed.

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,47 @@
 # vi: set ft=ruby :
 VAGRANTFILE_API_VERSION = "2"
 
+$provision_fedora = <<SCRIPT
+echo Provisioning SMF on Fedora
+
+export PYTHONUNBUFFERED=1
+echo '/swapfile none swap defaults 0 0' | sudo tee -a /etc/fstab
+sudo dnf upgrade -y
+sudo dnf install -y git htop
+
+cd /vagrant/meta
+
+echo $(pwd)
+. source_ansible_bash
+nice -n 19 ansible-playbook playbooks/devbox_all.yml
+
+SCRIPT
+
+$provision_ubuntu = <<SCRIPT
+echo Provisioning SMF on Ubuntu
+
+export PYTHONBUFFERED=1
+echo '/swapfile none swap defaults 0 0' | sudo tee -a /etc/fstab
+sudo apt-get update
+sudo apt-get install -y git htop
+
+cd /vagrant/meta
+echo $(pwd)
+. source_ansible_bash
+nice -n 19 ansible-playbook playbooks/devbox_all.yml
+
+SCRIPT
+
+SYSTEM= ENV.fetch("SMF_SYSTEM", "fedora").downcase
+case SYSTEM
+when "fedora"
+    BOX="fedora/25-cloud-base"
+    PROVISION_SCRIPT=$provision_fedora
+when "ubuntu"
+    BOX="ubuntu/trusty64"
+    PROVISION_SCRIPT=$provision_ubuntu
+end
+
 $provision_smf = <<SCRIPT
 echo Provisioning SMF
 
@@ -21,14 +62,15 @@ SCRIPT
 git_branch=`git rev-parse --abbrev-ref HEAD `
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.hostname = "smurf" + "." + git_branch.gsub(/[\/,:()\s_]/ , '.')
-  config.vm.box = "fedora/25-cloud-base"
+  config.vm.box = BOX
+  config.vm.box_check_update = false
   config.ssh.keep_alive = true
   # From https://fedoraproject.org/wiki/Vagrant, setup vagrant-hostmanager
   if Vagrant.has_plugin? "vagrant-hostmanager"
       config.hostmanager.enabled = true
       config.hostmanager.manage_host = true
   end
-  config.vm.provision "shell", inline: $provision_smf
+  config.vm.provision "shell", inline: PROVISION_SCRIPT
   config.vm.provider :virtualbox do |vb|
     vb.memory = 8096
     vb.cpus = 4
--- misc/provision_vagrant.sh
+++ misc/provision_vagrant.sh
@@ -2,9 +2,73 @@
 
 # Copyright 2017 Alexander Gallego
 #
-
-
 set -ex
+
+function die_unsupported {
+    local system="$1"
+    echo "Unsupported system: $system" && exit 1
+}
+
+function install_brew {
+    local system="$1"
+    if [[ -z $(which brew) ]]; then
+        case $system in
+            darwin)
+                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+                ;;
+            *)
+                # skip brew on this platform
+                ;;
+        esac
+    fi
+}
+
+function install_vagrant {
+    local system="$1"
+    if [[ -z $(which vagrant) ]]; then
+        case $system in
+            fedora)
+                sudo dnf install vagrant vagrant-libvirt vagrant-libvirt-doc vagrant-lxc
+                ;;
+            ubuntu)
+                sudo apt-get install vagrant
+                ;;
+            darwin)
+                brew cask install vagrant
+                ;;
+            *)
+                die_unsupported $system
+                ;;
+        esac
+    fi
+}
+
+function install_virtualbox {
+    local system="$1"
+    if [[ -z $(which VirtualBox) ]]; then
+        case $system in
+            fedora)
+                pushd /etc/yum.repos.d/
+                ## Fedora 25/24/23/22/21/20/19/18/17/16 users
+                sudo bash -c 'wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | tee '
+                popd
+                sudo dnf update
+                sudo dnf install binutils gcc make patch libgomp glibc-headers glibc-devel \
+                     kernel-headers kernel-PAE-devel dkms libffi-devel ruby-devel VirtualBox
+                ;;
+            ubuntu)
+                sudo apt-get install virtualbox
+                ;;
+            darwin)
+                brew cask install virtualbox
+                ;;
+            *)
+                die_unsupported $system
+                ;;
+        esac
+    fi
+}
+
 echo "usage: provision_vagrant.sh (debug|quiet)?"
 if [[ $1 == "debug" ]]; then
     export VAGRANT_LOG=info
@@ -20,29 +84,16 @@ fi
 git_root=$(git rev-parse --show-toplevel)
 cd $git_root
 
-if [[ $system == "fedora" ]]; then
-    if [[ $(which vagrant) == "" ]]; then
-        sudo dnf install vagrant vagrant-libvirt vagrant-libvirt-doc vagrant-lxc
-    fi
-    if [[ $(which VirtualBox) == "" ]]; then
-        pushd /etc/yum.repos.d/
-        ## Fedora 25/24/23/22/21/20/19/18/17/16 users
-        sudo bash -c 'wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | tee '
-        popd
-        sudo dnf update
-        sudo dnf install binutils gcc make patch libgomp glibc-headers glibc-devel \
-             kernel-headers kernel-PAE-devel dkms libffi-devel ruby-devel VirtualBox
-    fi
-fi
-
-if [[ $system == "darwin" ]]; then
+if [[ system == "darwin" ]]; then
     if [[ $(which brew) == "" ]]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
-    if [[ $(which vagrant) == "" ]]; then
-        brew cask install vagrant virtualbox
-    fi
 fi
+
+install_brew $system
+install_virtualbox $system
+install_vagrant $system
+
 if [[ $(vagrant plugin list | grep vbguest) == "" ]]; then
     vagrant plugin install vagrant-vbguest
 fi
